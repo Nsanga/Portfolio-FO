df43ae0c3fa2659df2d489fed6b9501e71b759a2
Mercure Mekinda <mercuremekinda@gmail.com>
2023-03-21 15:48:44 +0100

    Deploiement du back office

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors())
 
 // Start the app predictfoot
-app.listen(process.env.PORT || 8000, function() {
+app.listen(process.env.PORT || 9000, function() {
     console.log("app portfolio mekinda mercure live  started")
 })
